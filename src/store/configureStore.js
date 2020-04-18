b8109cc08982fda559a242167b6a96d99909e458
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-04-18 10:33:33 +0600

    Реализовал предварительную настройку store

---
+++ src/store/configureStore.js
@@ -0,0 +1,28 @@
+import {createBrowserHistory} from "history";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import {connectRouter, routerMiddleware} from "connected-react-router";
+import thunk from "redux-thunk";
+import usersReducer from "./reducers/usersReducer";
+import {loadFromLocalStorage, localStorageMiddleware} from "./localStorage";
+
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+  users: usersReducer
+});
+
+const middlewares = [
+  thunk,
+  routerMiddleware(history),
+  localStorageMiddleware
+];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(applyMiddleware(...middlewares));
+
+const persistedState = loadFromLocalStorage();
+
+export const store = createStore(rootReducer, persistedState, enhancers);
